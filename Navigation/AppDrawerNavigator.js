0b93831778d59872c8db223e638886415d297cf8
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-03-09 19:45:42 +0500

    Add files via upload

--- Navigation/AppDrawerNavigator.js
+++ Navigation/AppDrawerNavigator.js
@@ -1,18 +1,68 @@
 import React, { Component }  from 'react';
 import {TouchableOpacity, View} from 'react-native';
-import {createDrawerNavigator, createStackNavigator, createSwitchNavigator} from 'react-navigation';
+import {createDrawerNavigator, createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 import DashboardScreen from '../screens/Dashboard/Dashboard';
 import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import createCircle from '../screens/createCircle/createCircle';
 import joinCircle from '../screens/joinCircle/joinCircle';
 import signOut from '../screens/signOut/signOut';
 import myCircle from '../screens/myCircle/myCircle';
+import viewCircle from '../screens/viewCircle/viewCircle';
 
+const AppTabNavigator = createBottomTabNavigator({
+    My_Circles: {
+    screen:myCircle,
+    navigationOptions: {
+        tabBarLabel:"My Circles",
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialCommunityIcons name="account-group" size={30} color="#2b9077" />
+          ),
+        tabBarOptions: { activeTintColor:'#2b9077', },
+      },
+    },
+    // Offer: Offers,
+    Map: {
+    screen:viewCircle,
+    navigationOptions: {
+        tabBarLabel:"Map",
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialCommunityIcons name="map-marker" size={30} color="#2b9077" />
+        ),
+        tabBarOptions: { activeTintColor:'#2b9077', },
+      },
+    },
+    // Map: Map,
+    // Category: Category,
+    // Chat: Chat,
+    // UserProfile: UserProfile,
+    // Offer:Offers
+})
+
+const AppStackNavigator = createStackNavigator({
+    circle: {
+        screen: AppTabNavigator,
+        navigationOptions: ({navigation}) => ({
+            headerLeft:(
+                <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                    <View style={{paddingHorizontal: 10}}> 
+                        <Icon name="md-menu" size={24} />
+                    </View>
+                </TouchableOpacity>
+            )
+        })
+    }
+
+})
 
 const AppDrawerNavigator = createDrawerNavigator({
-    My_circle: createStackNavigator({
+    
+
+    My_circle: AppStackNavigator,
+
+    Map: createStackNavigator({
         circle: {
-            screen: myCircle,
+            screen: viewCircle,
             navigationOptions: ({navigation}) => ({
                 headerLeft:(
                     <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
@@ -24,6 +74,7 @@ const AppDrawerNavigator = createDrawerNavigator({
             })
         }
     }),
+
     Dashboard: createStackNavigator({
         Dashboard: {
             screen: DashboardScreen,
